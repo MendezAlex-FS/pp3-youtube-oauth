3b0ee05ebea8c16cba507be1d39e6812aba50909
Alex Mendez <AMendez4@student.fullsail.edu>
2026-02-08 21:19:09 -0500

    Setup your project repository Fixes #1

---
+++ server/index.js
@@ -0,0 +1,49 @@
+// Load in our Express framework
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+
+// Create a new Express instance called "app"
+const app = express();
+
+// This is the middleware
+// Make app parse request in JSON
+app.use(express.json()); // Put all data in req.body
+app.use(cors()); // Handles the CORs policies
+
+// Making at '/api/v1' as root service up routes
+app.get('/api/v1', (req, res) => {
+  console.log('GET');
+  res.status(200).json({
+    message: "Service is up",
+    metadata: {
+      hostname: req.hostname,
+      path: req.originalUrl,
+      protocol: req.protocol,
+      method: req.method,
+      date: new Date()
+    },
+  });
+});
+
+// It is a 404 handler
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+// Catch all errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    status: err.status
+  });
+});
+
+// Set our app to listen on port 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
